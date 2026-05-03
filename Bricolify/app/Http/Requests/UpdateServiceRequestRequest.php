b1192b486d662f6d0d33a72a0d475e2969d3d85d
mohamed-elhamdaoui <mohamedelhamdaoui007@gmail.com>
2026-05-03 18:44:04 +0100

    Create app/Http/Requests/UpdateServiceRequestRequest.php

---
+++ Bricolify/app/Http/Requests/UpdateServiceRequestRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateServiceRequestRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return auth()->guard()->check();
+    }
+
+    public function rules()
+    {
+        return [
+            'category_id'    => ['required', 'exists:categories,id'],
+            'title'          => ['required', 'string', 'max:255'],
+            'description'    => ['required', 'string', 'max:2000'],
+            'location'       => ['required', 'string', 'max:255'],
+            'scheduled_date' => ['required', 'date', 'after_or_equal:today'],
+            'image'          => ['nullable', 'image', 'mimes:jpeg,png,jpg', 'max:5120'],
+        ];
+    }
+}
