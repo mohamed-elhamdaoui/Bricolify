59885b5156ec8b0885739d3e18e07390934732e6
mohamed-elhamdaoui <mohamedelhamdaoui007@gmail.com>
2026-05-04 00:35:04 +0100

    config: register active_worker middleware alias

--- Bricolify/bootstrap/app.php
+++ Bricolify/bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
+            'active_worker' => \App\Http\Middleware\EnsureWorkerIsActive::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
